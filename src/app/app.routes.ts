d0e69cf5023493c225e42e1f42646ee0761e7dfc
Jason Lee <jaeho52lee@gmail.com>
2024-09-01 19:31:41 -0500

    added loading page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { loadScreenComponent } from './loadScreen/loadScreen.component';
+import { smallGameComponent } from './smallGame/smallGame.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", 
+        title: "Landing Page", 
+        component: loadScreenComponent
+    }, 
+    {
+        path: "small-game",
+        title: "Small Game",
+        component: smallGameComponent
+    }
+];
---
+++ src/app/loadScreen/loadScreen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'loadScreen',
+  templateUrl: './loadScreen.component.html',
+  styleUrls: ['./loadScreen.component.css']
+})
+export class loadScreenComponent {
+
+}
