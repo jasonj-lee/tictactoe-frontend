233b8c1d788783b501ce2f932cdd17eebae5a50d
Jason Lee <jaeho52lee@gmail.com>
2024-10-07 10:06:28 -0500

    beautified leaderboard stats

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -4,6 +4,15 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { UserService } from '../user.service';
 
+class User {
+    constructor (
+        public name: string, 
+        public wins: number, 
+        public gamesPlayed: number, 
+        public winRate: string
+    ) {};
+}
+
 @Component({
   selector: 'leaderboard',
   imports: [RouterModule, HttpClientModule, CommonModule],
@@ -19,7 +28,11 @@ export class LeaderboardComponent {
     ngOnInit() {
         this.userService.getUsers().subscribe({
         next: (response) => {
-            this.users = response;
+            this.users = response.map(
+                (userElem: any) => new User(userElem.name, userElem.wins, userElem.gamesPlayed, 
+                    (userElem.gamesPlayed > 0) ? (Math.round(((userElem.wins / userElem.gamesPlayed) + Number.EPSILON) * 1000) / 1000).toFixed(3) : "0.000"
+                )
+            )
         },
         error: (err) => console.error('Error fetching users:', err),
         });
