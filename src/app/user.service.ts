f8665a354f498ea13b7d9a1171468813cbe398a5
Jason Lee <jaeho52lee@gmail.com>
2024-10-06 21:47:44 -0500

    corrected link to backend

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',  // Automatically makes this service available globally
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3001/users';
+  private apiUrl = 'http://localhost:3001/user';
 
   constructor(private http: HttpClient) {}
 
